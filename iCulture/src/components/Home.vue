8ecc7d636098abca4ca734017b47ba86258a5609
TaikiKoyama <taikikoyama0701@gmail.com>
2018-10-12 11:59:04 +1000

    create home page

--- iCulture/src/components/Home.vue
+++ iCulture/src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="home frame">
+        <!-- Title page -->
         <div v-if="cover" class="cover">
             <div class="logo">
                 <img src="../assets/logo.png">
@@ -8,14 +9,65 @@
             <div class="text">Find new Culture</div>
             <button @click="cover = false">Continue</button>
         </div>
+
+        <!-- Home page -->
+        <div v-else class="main">
+
+            <!-- Header -->
+            <div class="header">
+                <div
+                 class="switch"
+                 :class="{active: window == 'home'}"
+                 @click="window = 'home'"
+                >Home</div>
+                <div
+                 class="switch"
+                 :class="{active: window == 'events'}"
+                 @click="window = 'events'"
+                >Events</div>
+            </div>
+
+            <!-- Search Bar -->
+            <div class="search">
+                <img src="../assets/search.svg">
+                <input placeholder="Find New Culture!">
+            </div>
+
+            <!-- Home Events -->
+            <div
+             class="main-window"
+             v-if="window == 'home'"
+            >
+                <!-- Popular Events -->
+                <EventSlide :type="'Popular Events'"></EventSlide>
+
+                <!-- Events Near You -->
+                <EventSlide :type="'Events Near You'"></EventSlide>
+
+                <!-- Upcoming Events -->
+                <EventSlide :type="'Upcoming Events'"></EventSlide>
+            </div>
+
+            <!-- All Events -->
+            <div
+             class="main-window"
+             v-if="window == 'events'"
+            >
+            Events
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import EventSlide from '@/components/items/EventSlide'
+
 export default {
     name: "Home",
+    components: { EventSlide },
     data: () => ({
         cover: true,
+        window: "home"
     })
 }
 </script>
@@ -23,7 +75,7 @@ export default {
 <style lang="scss" scoped>
 .home {
     
-
+    // Title Page
     .cover {
         display: flex;
         align-items: center;
@@ -53,12 +105,71 @@ export default {
             width: 275px;
             height: 40px;
             margin-top: 200px;
-            border: 1px solid #000000;
+            border: 0;
             border-radius: 4px;
             background-color: #f2bb05;
             color: #FFFFFF;
             font-size: 24px;
-            font-family: "Georgia";
+            font-family: "Arial";
+            box-shadow: 0 0 8px #f2bb05;
+        }
+    }
+
+    // Home Page
+    .main {
+        width: 100%;
+        height: 100%;
+        font-family: "Arial";
+        
+        .header {
+            display: flex;
+            width: 100%;
+            height: 60px;
+
+            .switch {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width: 50%;
+                background-color: #d74e09;
+                color: #FFFFFF;
+                font-size: 30px;
+                box-shadow: 0 0 15px #6e0e0a inset;
+                z-index: 2;
+
+                &.active{
+                    box-shadow: none;
+                    border-bottom: 5px solid #6e0e0a;
+                }
+            }
+        }
+
+        .search {
+            display: flex;
+            width: 100%;
+            height: 40px;
+            background-color: #FFFFFF;
+            box-shadow: 0 0 4px #000000;
+            z-index: 1;
+            margin-bottom: 10px;
+
+            img {
+                height: 20px;
+                width: 20px;
+                padding: 10px;
+            }
+
+            input {
+                border: 0;
+                font-size: 18px;
+                padding-left: 10px;
+            }
+        }
+
+        .main-window {
+            height: calc(100% - 130px);
+            padding: 10px;
+            overflow: scroll;
         }
     }
 }
---
+++ iCulture/src/components/items/EventSlide.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="event-slide">
+        <div class="title">{{ type }}</div>
+        <div class="slide">
+            <div class="card" v-for="(card, index) in sample_data" :key="index">
+                <img :src="card.img_path">
+                <span>{{ card.name }}</span>
+                <div
+                 v-if="type == 'Popular Events'"
+                 class="info"
+                >
+                    <img src="../../assets/hearts.svg">
+                    {{ card.likes }}
+                </div>
+                <div
+                 v-if="type == 'Events Near You'"
+                 class="info"
+                >
+                    {{ card.location }}
+                </div>
+                <div
+                 v-if="type == 'Upcoming Events'"
+                 class="info"
+                >
+                    {{ card.date }}
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "EventSlide",
+    props: ["type"],
+    data: () => ({
+        sample_data: [
+            {
+                name: "Chinese New Year",
+                likes: 609,
+                img_path: require("../../assets/sample_events/ChineseNewYear/sample01.jpg"),
+                location: "St Lucia(0.7km)",
+                date: "16 February 2018"
+            },
+            {
+                name: "Songkran",
+                likes: 338,
+                img_path: require("../../assets/sample_events/Songkran/sample01.jpg"),
+                location: "Toowong(2.3km)",
+                date: "16 February 2018"
+            },
+            {
+                name: "St. Patrick's Day",
+                likes: 427,
+                img_path: require("../../assets/sample_events/StPatricksDay/sample01.jpg"),
+                location: "Indooroopilly(7.8km)",
+                date: "27 December 2018"
+            }
+        ]
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+.event-slide {
+    margin-bottom: 20px;
+
+    .title {
+        font-size: 24px;
+        font-weight: bold;
+        color: #f2bb05;
+        margin-bottom: 10px;
+    }
+
+    .slide {
+        display: flex;
+        width: 100%;
+        overflow: scroll;
+        padding-bottom: 10px;
+        box-shadow: 0px 4px 3px -3px rgba(0,0,0,0.5);
+
+        .card {
+            display: flex;
+            margin-right: 15px;
+            flex-direction: column;
+            color: #d74e09;
+
+            > img {
+                width: 140px;
+                height: 80px;
+                margin-bottom: 10px;
+            }
+
+            span {
+                font-weight: bold;
+                font-size: 16px;
+            }
+
+            .info {
+                display: flex;
+                align-items: center;
+                font-size: 14px;
+
+                img {
+                    width: 16px;
+                    height: 16px;
+                    margin-right: 8px;
+                }
+            }
+        }
+    }
+}
+</style>
+
+
